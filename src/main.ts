957a5915df8eb778efa2a4c8f41afbd67389d1fd
1445654576@qq.com <1445654576@qq.com>
2022-01-02 23:43:29 +0800

    fix: 去除npm包

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {FlexBox} from './components';
-import AFlexBox from '@alanlee97/vue3-flex-box'; // 上传到npm的flex-box包
-import '@alanlee97/vue3-flex-box/dist/style.css';
+// import AFlexBox from '@alanlee97/vue3-flex-box'; // 上传到npm的flex-box包
+// import '@alanlee97/vue3-flex-box/dist/style.css';
 
 const app = createApp(App);
 app.component('flex-box', FlexBox);
-app.component('a-flex-box', AFlexBox);
+// app.component('a-flex-box', AFlexBox);
 app.mount('#app')
