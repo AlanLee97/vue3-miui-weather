47ecf1ccfafcfefe465d43520208e697f0bb1560
AlanLee <1445654576@qq.com>
2022-01-05 22:44:07 +0800

    feat: 完成提示消息模块

--- src/components/tip-box-item/tip-box-item.vue
+++ src/components/tip-box-item/tip-box-item.vue
@@ -1,14 +1,37 @@
 <script lang="ts" setup>
+
+export interface TipBoxItemData {
+  icon: string,
+  text: string
+}
+const props = defineProps<{
+  data: TipBoxItemData
+}>();
+
+let {
+  icon = '../../assets/clothing.png',
+  text = '文本'
+} = props.data;
+
 </script>
 
 <template>
-  <div class="tip-box-item">
-    提示信息Item
-  </div>
+  <flex-box class="tip-box-item" direction-column center-h-v>
+    <img src="../../assets/clothing.png" alt="" class="icon">
+    <div class="text">{{text}}</div>
+  </flex-box>
 </template>
 
 <style lang="scss" scoped>
 .tip-box-item {
-
+  .icon {
+    width: 40px;
+    height: 40px;
+  }
+  .text {
+    font-size: 12px;
+    color: #FFF;
+    margin-top: 16px;
+  }
 }
 </style>
--- src/components/tip-box/tip-box.vue
+++ src/components/tip-box/tip-box.vue
@@ -1,14 +1,60 @@
 <script lang="ts" setup>
+import { reactive } from '@vue/reactivity';
+import { TipBoxItem } from '..';
+import { TipBoxItemData } from '../tip-box-item/tip-box-item.vue';
+
+let tipBoxData1: Array<TipBoxItemData> = reactive([
+  {
+    icon: '',
+    text: '适宜薄外套',
+  },
+  {
+    icon: '',
+    text: '紫外线较弱',
+  },
+  {
+    icon: '',
+    text: '宜户外运动',
+  }
+])
+
+let tipBoxData2: Array<TipBoxItemData> = reactive([
+  {
+    icon: '',
+    text: '适宜薄洗车',
+  },
+  {
+    icon: '',
+    text: '不用带伞',
+  },
+  {
+    icon: '',
+    text: '较易感冒',
+  }
+])
 </script>
 
 <template>
-  <div class="tip-box">
-    提示信息
-  </div>
+  <flex-box class="tip-box" space-between style="flex-wrap: wrap;">
+    <template v-for="item in tipBoxData1" :key="item.text">
+      <tip-box-item :data="item" class="item-wrapper" />
+    </template>
+    <div style="width: 100%; height: 20px;"></div>
+    <template v-for="item in tipBoxData2" :key="item.text">
+      <tip-box-item :data="item" class="item-wrapper" />
+    </template>
+  </flex-box>
 </template>
 
 <style lang="scss" scoped>
 .tip-box {
+  padding: 20px;
+  margin: 20px;
+  background-color: rgba($color: #FFF, $alpha: .3);
+  border-radius: 20px;
 
+  .item-wrapper {
+    width: 28%;
+  }
 }
 </style>
