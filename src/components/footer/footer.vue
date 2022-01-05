c78c410470f8ccbe43c06e7d6d8df94f14e2394c
AlanLee <1445654576@qq.com>
2022-01-05 22:02:35 +0800

    feat: 完成footer组件

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,13 +2,19 @@
 </script>
 
 <template>
-  <div class="footer">
-    底部信息
-  </div>
+  <flex-box class="footer" center-h-v>
+    部分数据数据来自 <img src="../../assets/caiyun.png" class="icon" />彩云天气
+  </flex-box>
 </template>
 
 <style lang="scss" scoped>
 .footer {
-
+  font-size: 12px;
+  color: rgba(255,255,255, .5);
+  padding: 10px 0;
+  .icon {
+    width: 16px;
+    height: 16px;
+  }
 }
 </style>
