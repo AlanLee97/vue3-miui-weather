3194469d944ed7caf99166400e122690ceaae223
1445654576@qq.com <1445654576@qq.com>
2022-01-07 21:26:26 +0800

    fix: 完善天气预报界面

--- src/components/forecast/forecast.vue
+++ src/components/forecast/forecast.vue
@@ -3,7 +3,8 @@
 
 <template>
   <flex-box class="forecast" center-h-v>
-    天气预报
+    <!-- 天气预报 -->
+    <img src="../../assets/forecast_poster.webp" class="poster" />
   </flex-box>
 </template>
 
@@ -13,5 +14,14 @@
   border-radius: 20px;
   margin: 20px;
   background-color: rgba($color: #fff, $alpha: .3);
+  position: relative;
+
+  .poster {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
