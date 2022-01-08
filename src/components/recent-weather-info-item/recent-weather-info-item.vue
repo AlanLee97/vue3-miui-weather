e1d560714fcc1a57c2ccdfedd21a13b057a2b76d
1445654576@qq.com <1445654576@qq.com>
2022-01-08 13:29:18 +0800

    fix: 更新综合天气信息样式

--- src/components/recent-weather-info-item/recent-weather-info-item.vue
+++ src/components/recent-weather-info-item/recent-weather-info-item.vue
@@ -10,7 +10,10 @@ defineProps<{
   <div class="recent-weather-info-item">
     <flex-box space-between center-h-v>
       <flex-box class="left" center-h-v>
-        <img class="icon" src="../../assets/icon_cloudy.webp" alt="">
+        <img class="icon" src="../../assets/icon_cloudy.webp" alt="" v-if="data.maxTemperature || data.minTemperature">
+        <flex-box v-else class="icon-wrapper" center-h-v>
+          <img class="icon" src="../../assets/realtime_aqi_leaf.webp" alt="">
+        </flex-box>
         <template v-if="data.airStatus">
           <span class="desc-text">{{data.airStatus + ' '}}</span>
           <span>{{data.airQuality}}</span>
@@ -23,8 +26,9 @@ defineProps<{
       <flex-box class="right" center-h-v v-if="data.maxTemperature || data.minTemperature">
         {{data.maxTemperature}}° / {{data.minTemperature}}°
       </flex-box>
-      <flex-box v-else>
-        {{data.rainRate}}
+      <flex-box v-else class="rain-rate" center-v>
+        <flex-box class="icon-wrapper" center-h-v><img src="../../assets/rain-drop.png" alt="" class="icon"></flex-box>
+        <span class="rain-rate-text">{{data.rainRate}}</span>
       </flex-box>
     </flex-box>
   </div>
@@ -33,7 +37,7 @@ defineProps<{
 <style lang="scss" scoped>
 .recent-weather-info-item {
   padding: 0 20px;
-  line-height: 3;
+  line-height: 3.2;
   .left {
     .icon {
       width: 24px;
@@ -58,6 +62,28 @@ defineProps<{
 
   .right {
 
+  }
+
+  .icon-wrapper {
+    background: rgba($color: #fff, $alpha: .3);
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    .icon {
+      width: 12px;
+      height: 12px;
+    }
+  }
+
+  .rain-rate {
+    font-size: 14px;
+    .icon {
+      width: 12px;
+      height: 12px;
+    }
+    .rain-rate-text {
+      margin-left: 10px;
+    }
   }
 }
 </style>
