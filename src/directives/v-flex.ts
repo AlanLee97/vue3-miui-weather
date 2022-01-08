5192ecb332d13e10405a44d59ef7c29984532163
1445654576@qq.com <1445654576@qq.com>
2022-01-08 19:58:21 +0800

    fix: flex指令增加部分属性别名

--- src/directives/v-flex.ts
+++ src/directives/v-flex.ts
@@ -2,13 +2,34 @@ import { DirectiveBinding } from 'vue';
 import { transformCamelToDash } from '../utils';
 import './style.scss';
 
+const handleAliasProps = (alias: string) => {
+  let className = alias;
+  switch(className) {
+    case 'wrap':
+      className = 'flex-wrap';
+      break;
+    case 'direction-r':
+    case 'horizontal':
+      className = 'direction-row';
+      break;
+    case 'direction-c':
+    case 'vertical':
+      className = 'direction-column';
+      break;
+  }
+  if(['between', 'around', 'evenly'].includes(className)) {
+    className = `space-${className}`
+  }
+  return className;
+}
+
 export default {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     el.setAttribute('class', 'flex-box');
     const {modifiers} = binding;
     let currentClass = el.getAttribute('class') ?? '';
     for (const key in modifiers) {
-      currentClass += (' ' + key);
+      currentClass += (' ' + handleAliasProps(key));
     }
     el.setAttribute('class', transformCamelToDash(currentClass));
   }
