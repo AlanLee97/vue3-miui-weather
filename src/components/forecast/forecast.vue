54ddcaf382044b7fb982c5775eb9cd6eb9c98d1e
1445654576@qq.com <1445654576@qq.com>
2022-01-08 13:48:13 +0800

    fix: 更新天气预报样式

--- src/components/forecast/forecast.vue
+++ src/components/forecast/forecast.vue
@@ -10,16 +10,10 @@
 
 <style lang="scss" scoped>
 .forecast {
-  height: 160px;
   border-radius: 20px;
   margin: 20px;
-  background-color: rgba($color: #fff, $alpha: .3);
-  position: relative;
 
   .poster {
-    position: absolute;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
   }
