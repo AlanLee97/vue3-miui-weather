af93a05d7b00401aa3e9c51f803f1238391d9193
1445654576@qq.com <1445654576@qq.com>
2022-06-12 03:07:04 +0800

    fix: 调整项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+// import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // 解决打包后显示空白问题（本质上是指定资源路径为当前目录）
-  plugins: [vue(), eslintPlugin({
-    include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
-  })]
-})
+  plugins: [vue()],
+  // plugins: [vue(), eslintPlugin({
+  //   include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
+  // })]
+});
